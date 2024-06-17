430db2b9f3621225785801d89104b19fa496f3a8
EdMadd <martl.eichinger@gmail.com>
2024-06-17 14:04:29 +0200

    test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 //import reportWebVitals from './reportWebVitals';
-//add comment
+//add comment add
 
 ReactDOM.render(
 	<React.StrictMode>
